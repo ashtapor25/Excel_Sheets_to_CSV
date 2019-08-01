a091a23fdf2a1197432eaafe3460d1a20df5efcd
ashtapor25 <junsushin4546@gmail.com>
2019-08-01 19:01:45 +0900

    special character deletion for sheet names -> csv file anme and xlrd import modification

--- excel_sheets_to_csv.py
+++ excel_sheets_to_csv.py
@@ -5,10 +5,12 @@ import subprocess
 import csv
 print("\n<Excel_Sheet_to_CSV> by.Junsu Shin @https://github.com/ashtapor25\n")
 
-print("Installing Required Packages...")
-subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'xlrd'])
-
-import xlrd
+try:
+    import xlrd
+except ImportError:
+    print("Installing Required Packages...")
+    subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'xlrd'])
+    import xlrd
 
 def excel_to_csv():
     # take the file name as input
@@ -20,20 +22,25 @@ def excel_to_csv():
         print(target_file+" does not exist in the directory")
         return
 
+    no_filename_chars = ['<', '>', '?', '[', ']', ':', '|', '*']
+    transtable = str.maketrans('','','<>?[]:|*')
+
     # create a folder in the home directory for the CSVs
     folder_name = target_file.replace(".xlsx", "").replace(".xls", "")
     os.mkdir(folder_name)
     print("folder "+folder_name+" created in directory")
 
+    print('characters <>?[]:|* will be removed from sheet names when making csv files')
     # create a CSV file for every sheet
     with xlrd.open_workbook(target_file) as wb:
         sheet_lst = wb.sheets()
         for sheet in sheet_lst:
-            with open(folder_name+"/"+sheet.name+'.csv', 'w', newline="") as f:
+            sheetname = sheet.name.translate(transtable)
+            with open(folder_name+"/"+sheetname+'.csv', 'w', newline="") as f:
                 c = csv.writer(f)
                 for r in range(sheet.nrows):
                     c.writerow(sheet.row_values(r))
-            print(sheet.name+'.csv created')
+            print(sheetname+'.csv created')
 
     return
 
